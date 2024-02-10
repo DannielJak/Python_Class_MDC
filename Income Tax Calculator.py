d9476a0ec5a4258c7e07b37a5598ec7220b4426e
DannielJak <159641305+DannielJak@users.noreply.github.com>
2024-02-10 13:21:19 -0500

    Add files via upload

---
+++ Income Tax Calculator.py
@@ -0,0 +1,10 @@
+
+gross_income = float(input("Enter the gross income: ", ))
+gross_income = round(gross_income, 2)
+number_of_dependents = int(input("Enter the number of dependents: ", ))
+
+taxable_income = gross_income - 10000
+taxable_income -= (3000 * number_of_dependents)
+income_tax = (taxable_income * 20) / 100
+
+print(f'The income tax is ${income_tax}')
